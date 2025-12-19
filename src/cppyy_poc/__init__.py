1ae00fb5d8bcfe85206f4feeee81101088933bc5
Legend101Zz <96632943+Legend101Zz@users.noreply.github.com>
2025-12-19 19:46:31 +0530

    refactor : init file

--- src/cppyy_poc/__init__.py
+++ src/cppyy_poc/__init__.py
@@ -4,19 +4,33 @@ Brian2 cppyy Proof of Concept.
 A minimal demonstration of how cppyy could replace Cython in Brian2.
 """
 
+from .clock import Clock
 from .codegen import CodeGenerator
 from .codeobject import CppyyCodeObject
+from .inspector import CppyyInspector
+from .network import Network
 from .neurongroup import NeuronGroup
-from .variables import ArrayVariable, Constant, Variable, VariableResolver
+from .statemonitor import StateMonitor
+from .variables import (ArrayVariable, Constant, DynamicArrayVariable,
+                        Variable, VariableResolver)
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 __all__ = [
+    # Core simulation objects
     'NeuronGroup',
+    'StateMonitor',
+    'Network',
+    'Clock',
+    # Variable system
     'Variable',
     'Constant',
     'ArrayVariable',
+    'DynamicArrayVariable',
     'VariableResolver',
+    # Code generation
     'CodeGenerator',
     'CppyyCodeObject',
+    # Debugging/Introspection
+    'CppyyInspector',
 ]
